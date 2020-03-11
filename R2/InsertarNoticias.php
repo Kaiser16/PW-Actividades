c8886de9d1b2b5d4fe1a40e68de2604c1d282210
Jesús Márquez <jesusmarquezdelgado@gmail.com>
2020-03-11 10:07:46 +0100

    Ejercicios

--- R2/InsertarNoticias.php
+++ R2/InsertarNoticias.php
@@ -4,28 +4,39 @@
 	<title>Insertar Noticia</title>
 </head>
 <body>
-	<?php 
-		if($_POST)
-		{
-			$dbhost = 'localhost';
+	<?php
+	$enviada = false; 
+	error_reporting(E_ALL & ~E_NOTICE);
+	$noticia = $_REQUEST['enviar'];
+	if(isset($noticia))
+	{
+		$enviada = true;
+		$dbhost = 'localhost';
 		$database = 'lindavista';
-		$user = 'root';
-		$pass = '';
+		$user = 'cursophp-ad';
+		$pass = 'php.hph';
 		$port = '3308';
 		$conexion = mysqli_connect($dbhost,$user,$pass,$database,$port) or die ("Fallo con la conexion a la bd");
 		mysqli_query($conexion,"INSERT INTO noticias (id_noticia,titulo,noticia) VALUES (NULL,'".$_POST['titulo']."','".$_POST['noticia']."')") or die ("Error al insertar");
 		mysqli_close($conexion);
-			exit();
-		}
+
+	}
 	?>
 	<h2>Insertar Nueva Noticia</h2>
 	<form method="post" action="InsertarNoticias.php">
 		Titulo:* <input type="text" name="titulo" required><br>
 		<br>Noticia:*
 		<br><textarea name='noticia'></textarea><br>
-		<br><input type="submit" value="Enviar Noticia"><br>
+		<br><input type="submit" name="enviar" value="Enviar Noticia"><br>
 		<hr><div class="footnote">Los campos con (*) deben ser completados</div>
 	</form>
-
+	<?php
+	if($enviada == true)
+	{
+		echo "<hr><div class=footnote><h3>Noticia enviada</h3></div>";
+		echo "Titulo: ".$_POST['titulo'];
+		echo "<br>Noticia: ".$_POST['noticia'];
+	}
+	?>
 </body>
 </html>
--- R2/Noticias.php
+++ R2/Noticias.php
@@ -4,44 +4,118 @@
 	<title>Noticias</title>
 </head>
 <body>
-	<h2>NOTICIAS</h2>
-	<?php
+	<form method="post" action="Noticias.php">
+		<input type="submit" name="pmostrar" value="Noticias">
+		<input type="submit" name="pinsertar" value="Insertar Noticia">
+	</form>
+
+	<?php  
 	$dbhost = 'localhost';
-	$database = 'Noticiario';
-	$user = 'root';
-	$pass = '';
+	$database = 'lindavista';
+	$user = 'cursophp-ad';
+	$pass = 'php.hph';
 	$port = '3308';
-	$conexion = mysqli_connect($dbhost,$user,$pass,$database,$port) or die ("Fallo con la conexion a la bd");
-	$res = mysqli_query($conexion,"SELECT * FROM noticias");
 	?>
-	<table border = 1>
-		<tr>
-			<td>
-				ID
-			</td>
-			<td>
-				TITULO
-			</td>
-			<td>
-				NOTICIA
-			</td>
-			<td>
-				BORRAR
-			</td>
-		</tr>
-		<?php
-		while($row = mysqli_fetch_assoc($res)){
-			echo "<tr>";
-				foreach ($row as $value) {
-					echo "<td>";
-					echo $value;
-					echo "</td>";
-				}
-			echo "</tr>";
-		}
-		?>
+
 	<?php
-	mysqli_close($conexion);
+	error_reporting(E_ALL & ~E_NOTICE);
+	$borrar = $_REQUEST['borrar']; 
+	if(isset($borrar)){
+		$conexion = mysqli_connect($dbhost,$user,$pass,$database,$port) or die ("Fallo con la conexion a la bd");
+		$res = mysqli_query($conexion,"SELECT * FROM noticias");
+		$idborrar = $_POST['eliminar'];
+		$nborrar = count($idborrar);
+		foreach ($idborrar as $value) {
+			$query = "delete from noticias where id_noticia = ".$value;
+			mysqli_query($conexion,$query) or die("F Borrar");
+		}
+		mysqli_close($conexion);
+	}
+	?>
+
+	<?php
+	$enviada = false; 
+	error_reporting(E_ALL & ~E_NOTICE);
+	$noticia = $_REQUEST['enviarn'];
+	if(isset($noticia))
+	{
+		$enviada = true;
+		$conexion = mysqli_connect($dbhost,$user,$pass,$database,$port) or die ("Fallo con la conexion a la bd");
+		mysqli_query($conexion,"INSERT INTO noticias (id_noticia,titulo,noticia) VALUES (NULL,'".$_POST['titulo']."','".$_POST['noticia']."')") or die ("Error al insertar");
+		mysqli_close($conexion);
+
+	}
+	?>
+
+	<?php
+	if(isset($_REQUEST['pinsertar']) || isset($_REQUEST['enviarn']))
+	{
+	?>
+		<h2>Insertar Nueva Noticia</h2>
+		<form method="post" action="Noticias.php">
+			Titulo:* <input type="text" name="titulo" required><br>
+			<br>Noticia:*
+			<br><textarea name='noticia'></textarea><br>
+			<br><input type="submit" name="enviarn" value="Enviar Noticia"><br>
+			<hr><div class="footnote">Los campos con (*) deben ser completados</div>
+		</form>
+	<?php
+		if($enviada == true)
+		{
+			echo "<hr><div class=footnote><h3>Noticia enviada</h3></div>";
+			echo "Titulo: ".$_POST['titulo'];
+			echo "<br>Noticia: ".$_POST['noticia'];
+		}
+	}
+	else
+	{
+	?>
+		<h2 align="center">NOTICIAS</h2>
+	<?php
+		$conexion = mysqli_connect($dbhost,$user,$pass,$database,$port) or die ("Fallo con la conexion a la bd");
+		$res = mysqli_query($conexion,"SELECT * FROM noticias");
+		$nrows = mysqli_num_rows($res);
+		if($nrows != 0)
+		{
+	?>
+		<form method="post" action="Noticias.php">
+		<table border = '1' align="center">
+			<tr>
+				<td>
+					TITULO
+				</td>
+				<td>
+					NOTICIA
+				</td>
+				<td>
+					BORRAR
+				</td>
+			</tr>
+	<?php
+			$i=1;
+			while($row = mysqli_fetch_assoc($res)){
+				echo "<tr>";
+					echo "<td>";
+					echo $row['titulo'];
+					echo "</td>";
+					echo "<td>";
+					echo $row['noticia'];
+					echo "</td>";
+					echo "<td>";
+						echo "<input type=checkbox name=eliminar[] value=".$row['id_noticia'].">";
+					echo "</td>";
+				echo "</tr>";
+				$i++;
+			}
+	?>
+		</table><br>
+		<input type="submit" name="borrar" value="Borrar">
+		</form>
+		<?php
+		}
+		else echo "No existen noticias";
+		mysqli_close($conexion);
+	}
 	?>
 </body>
 </html>
